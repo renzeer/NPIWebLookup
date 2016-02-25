9050015063ae4eae13a7b08a84c8073103b34501
renzeer <rj4219@hotmail.com>
2016-02-24 20:19:14 -1000

    Initial commit

---
+++ NPIWebLookup/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NPIWebLookup {
+    class Program {
+
+        [STAThread]
+        static void Main(string[] args) {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Please select Excel file containing NPI data";
+            ofd.Filter = "Excel Files|*.xls;*.xlsx;*.xlsm";
+
+            if (ofd.ShowDialog() != DialogResult.OK) {
+                return;
+            }
+
+            Console.WriteLine(ofd.FileName);
+            Console.ReadKey();
+
+        }
+    }
+}
